9715f56ac16c6eaf865dc743cfa8c4a8a83cedbf
wilo <sudowilo@gmail.com>
2025-07-25 19:14:14 +0330

    feat(auth): insert register info to database

--- src/server/controllers/authController.js
+++ src/server/controllers/authController.js
@@ -32,8 +32,9 @@ export const register = async (req, res) => {
     }
 
     const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
+    const hashPassword = await bcrypt.hash(password, salt);
 
+    //checking least securities of password
     if (password.includes(" ") || password.length < 8) {
       return res.status(400).json({
         success: false,
@@ -42,17 +43,45 @@ export const register = async (req, res) => {
       });
     }
 
+    const { data, error } = await supabase
+      .from("users")
+      .insert({
+        first_name: firstName,
+        last_name: lastName,
+        gender,
+        username,
+        password: hashPassword,
+        phone_number: phoneNumber,
+        email,
+      })
+      .select()
+      .single();
+
+    if (error) {
+      if (error.code === "23505") {
+        const { details } = error;
+        const conflicts = [];
+        if (details.includes("username")) conflicts.push("username");
+        if (details.includes("phone_number")) conflicts.push("phone_number");
+        if (details.includes("email")) conflicts.push("email");
+
+        return res.status(409).json({
+          success: false,
+          message: `user with same ${conflicts.join(", ")} already exists`,
+        });
+      }
+
+      console.log(error);
+      return res.status(500).json({
+        success: false,
+        message: "couldn't insert user to database",
+      });
+    }
+
     res.status(200).json({
       success: true,
       message: "ready to go",
-      data: {
-        firstName,
-        lastName,
-        gender,
-        username,
-        phoneNumber,
-        email,
-      },
+      data,
     });
   } catch (error) {
     console.error(error);
