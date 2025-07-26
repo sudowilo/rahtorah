c4763b6f44cd011a17fae109a30ad155c5a553b9
wilo <sudowilo@gmail.com>
2025-07-26 11:30:52 +0330

    feat(auth): auth middleware

--- src/server/middleware/authMiddleware.js
+++ src/server/middleware/authMiddleware.js
@@ -1,5 +1,31 @@
 import jwt from "jsonwebtoken";
 
+export const authMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      success: false,
+      message: "دسترسی غیرمجاز، توکن ارسال نشده است",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decode;
+
+    next();
+  } catch(error) {
+    console.error(error);
+    return res.status(401).json({
+      success: false,
+      message: "دسترسی غیرمجاز، توکن نامعتبر یا منقضی شده است"
+    })
+  }
+};
+
 export const checkAlreadyLoggedIn = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (authHeader?.startsWith("Bearer ")) {
