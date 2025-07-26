5007c2ebb5ad1c2a4dec4de78a0c61a2c4b210ce
wilo <sudowilo@gmail.com>
2025-07-26 20:07:48 +0330

    feat(trip): essential data about trip from user

--- src/server/controllers/tripController.js
+++ src/server/controllers/tripController.js
@@ -2,25 +2,39 @@ import { success } from "zod";
 import { validateCity } from "../validators/city.js";
 
 export const createTrip = async (req, res) => {
-  const { origin, destination } = req.body;
-  
+  const {
+    origin,
+    destination,
+    departureDate,
+    departureTimeFrom,
+    departureTimeTo,
+    suggestedTransportService,
+    seatsTotal,
+    allowedGender,
+    note,
+    user,
+  } = req.body;
+
   const validOrigin = validateCity(origin);
   const validDestination = validateCity(destination);
 
   if (!validOrigin) {
     return res.status(400).json({
       success: false,
-      message: "مبدا ارسال شده صحیح نمی‌باشد"
-    })
+      message: "مبدا ارسال شده صحیح نمی‌باشد",
+    });
   }
   if (!validDestination) {
     return res.status(400).json({
       success: false,
-      message: "مقصد ارسال شده صحیح نمی‌باشد"
-    })
+      message: "مقصد ارسال شده صحیح نمی‌باشد",
+    });
   }
 
   return res.json({
-    message: "hi there"
+    message: "hi there",
+    origin,
+    destination,
+    user,
   });
 };
