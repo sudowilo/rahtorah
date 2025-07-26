6546d9956aa4380d0ec1dbeb26c0847e2447de80
wilo <sudowilo@gmail.com>
2025-07-26 23:49:38 +0330

    feat(trip): adding trip poster to trips table

--- src/server/controllers/tripController.js
+++ src/server/controllers/tripController.js
@@ -1,3 +1,5 @@
+import { success } from "zod";
+import supabase from "../lib/supabaseClient.js";
 import { validateCity } from "../validators/city.js";
 import { tripSchema } from "../validators/trip.js";
 import dayjs from "dayjs";
@@ -48,7 +50,10 @@ export const createTrip = async (req, res) => {
   if (dateIsToday) {
     const from = dayjs(`${departureDate} ${departureTimeFrom}`, "HH:mm");
     const to = dayjs(`${departureDate} ${departureTimeTo}`, "HH:mm");
-    const nowTime = dayjs().set("second", 0).set("millisecond", 0).calendar("jalali");
+    const nowTime = dayjs()
+      .set("second", 0)
+      .set("millisecond", 0)
+      .calendar("jalali");
     console.log("from:", from);
     console.log("to:", to);
     console.log("nowTime:", nowTime);
@@ -60,11 +65,58 @@ export const createTrip = async (req, res) => {
     }
   }
 
+  const { data: hostUser, error: fetchError } = await supabase
+    .from("users")
+    .select()
+    .maybeSingle();
+
+  if (!hostUser) {
+    console.log(fetchError);
+    return res.status(500).json({
+      success: false,
+      message:
+        "خطای غیرمنتظره، حساب کاربری شما در دیتابیس وجود ندارد با پشتیبانی ارتباط برقرار کنید",
+    });
+  }
+
+  if (allowedGender !== hostUser.gender && allowedGender !== "any") {
+    return res.status(400).json({
+      success: false,
+      message:
+        'شما نمی‌توانید جنسیت مخالف خود را برای سفر انتخاب کنید از گزینه "مهم نیست" استفاده کنید',
+    });
+  }
+
+  const { data, error } = await supabase
+    .from("trips")
+    .insert({
+      creator_id: user.id,
+      origin_text: origin,
+      destination_text: destination,
+      departure_date: departureDate,
+      departure_time_from: departureTimeFrom,
+      departure_time_to: departureTimeTo,
+      suggested_transport_service: suggestedTransportService,
+      seats_total: seatsTotal,
+      allowed_gender: allowedGender,
+      note,
+      status: "open",
+    })
+    .select()
+    .maybeSingle();
+
+  if (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "خطای داخلی بعدا تلاش کنید",
+      error,
+    });
+  }
+
   return res.json({
     success: true,
-    message: "hi there",
-    origin,
-    destination,
-    user,
+    message: "آگهی سفر با موفقیت ثبت شد",
+    data,
   });
 };
