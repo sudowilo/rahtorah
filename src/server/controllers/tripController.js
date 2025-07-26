9cc594b9aa0502dc344b6d17272bf08c89c7c58b
wilo <sudowilo@gmail.com>
2025-07-26 23:06:18 +0330

    feat(trip): validate trip poster

--- src/server/controllers/tripController.js
+++ src/server/controllers/tripController.js
@@ -1,5 +1,9 @@
-import { success } from "zod";
 import { validateCity } from "../validators/city.js";
+import { tripSchema } from "../validators/trip.js";
+import dayjs from "dayjs";
+import jalali from "jalali-plugin-dayjs";
+dayjs.extend(jalali);
+const now = dayjs();
 
 export const createTrip = async (req, res) => {
   const {
@@ -15,6 +19,15 @@ export const createTrip = async (req, res) => {
     user,
   } = req.body;
 
+  const result = tripSchema.safeParse(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({
+      success: false,
+      message: "مشکل در اطلاعات ورودی",
+      errors: result.error.format(),
+    });
+  }
   const validOrigin = validateCity(origin);
   const validDestination = validateCity(destination);
 
@@ -31,7 +44,24 @@ export const createTrip = async (req, res) => {
     });
   }
 
+  const dateIsToday = dayjs(departureDate, { jalali: true }).isSame(now, "day");
+  if (dateIsToday) {
+    const from = dayjs(`${departureDate} ${departureTimeFrom}`, "HH:mm");
+    const to = dayjs(`${departureDate} ${departureTimeTo}`, "HH:mm");
+    const nowTime = dayjs().set("second", 0).set("millisecond", 0).calendar("jalali");
+    console.log("from:", from);
+    console.log("to:", to);
+    console.log("nowTime:", nowTime);
+    if (from.isBefore(nowTime) || to.isBefore(nowTime)) {
+      return res.status(400).json({
+        success: false,
+        message: "زمان حرکت باید بعد از زمان فعلی باشد",
+      });
+    }
+  }
+
   return res.json({
+    success: true,
     message: "hi there",
     origin,
     destination,
