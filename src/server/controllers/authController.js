38edbeb43616a3f4a0fbe65a751cde016970b757
wilo <sudowilo@gmail.com>
2025-07-27 19:00:40 +0330

    fix(auth) removing data from response to register

--- src/server/controllers/authController.js
+++ src/server/controllers/authController.js
@@ -89,7 +89,6 @@ export const register = async (req, res) => {
     res.status(200).json({
       success: true,
       message: "کاربر با موفقیت ثبت شد",
-      data,
       token,
     });
   } catch (error) {
