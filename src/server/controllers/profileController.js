119de1f4d00d17422570876245006819b5bd36ec
wilo <sudowilo@gmail.com>
2025-07-28 13:17:26 +0330

    feat(profile): requests api to get pending requests

--- src/server/controllers/profileController.js
+++ src/server/controllers/profileController.js
@@ -20,6 +20,37 @@ export const profileInfo = async (req, res) => {
   res.status(200).json({
     success: true,
     message: "اطلاعات پروفایل با موفقیت دریافت شد",
-    data
+    data,
+  });
+};
+
+export const requests = async (req, res) => {
+  const user = req.user;
+
+  const { data, error } = await supabase
+    .from("trip_participants")
+    .select(
+      `id,
+      role,
+      status,
+      trip_id!inner(id, status, creator_id, origin_text, seats_total, allowed_gender, departure_date, destination_text, departure_time_to, departure_time_from, suggested_transport_service), 
+      user_id!inner(id, username, last_name, first_name)`
+    )
+    .eq("trip_id.status", "open")
+    .eq("trip_id.creator_id", user.id)
+    .eq("status", "pending");
+
+  if (error) {
+    res.status(500).json({
+      success: false,
+      message: "خطای داخلی",
+      error,
+    });
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: "لیست درخواست های دریافتی",
+    data,
   });
 };
--- src/server/routes/profileRoutes.js
+++ src/server/routes/profileRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/authMiddleware.js";
-import {profileInfo} from "../controllers/profileController.js";
+import { profileInfo, requests } from "../controllers/profileController.js";
 const router = Router();
 
 router.get("/info", authMiddleware, profileInfo);
+router.get("/requests", authMiddleware, requests);
 
 export default router;
