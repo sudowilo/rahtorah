8e72e999d90e91e47d2850e5c0f44f1a3ba60db6
wilo <sudowilo@gmail.com>
2025-08-08 08:15:00 +0330

    feat(ui/user-card): changing the logic for rendering card

--- client/js/index.js
+++ client/js/index.js
@@ -1,25 +1,23 @@
 import { BASE_URL } from "./config.js";
 
-async function getUserCardHtml() {
-  const url =  `${BASE_URL}/render/userCard`;
+async function getUserInfo() {
+  const url = `${BASE_URL}/api/profile/info`;
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjhiNzBjZmM4LTYzNWUtNDM3Zi04NjIzLTI1NjE4ZjlhYmM0OSIsInVzZXJuYW1lIjoidGhlc3Bpcml0eCIsImlhdCI6MTc1MzYyOTk0MX0.mbJ4UbpvoTpB8A6B-cnFuBMhlIFWYT1JTPPX2kjaGlc`,
+      },
+    });
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
 
     const result = await response.json();
+    console.log(result);
     return result;
   } catch (error) {
     console.error(error.message);
   }
 }
 
-const renderUserCard = async () => {
-  const userCard = document.querySelector(".user-card");
-  const userCardHtml = await getUserCardHtml();
-
-  userCard.innerHTML = userCardHtml.data;
-};
-
-renderUserCard();
+getUserInfo();
--- server/routes/homePageRoutes.js
+++ server/routes/homePageRoutes.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
 import { uiAuthMiddleware } from "../middleware/authMiddleware.js";
-import { homePage, userCardRender } from "../controllers/homePageController.js";
+import { homePage } from "../controllers/homePageController.js";
 
 const router = new Router();
 
 router.get("/", homePage);
-
-router.get("/render/userCard", userCardRender);
 
 export default router;
