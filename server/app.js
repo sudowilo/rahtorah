b7104e05fb5aa11e01e62f70e2f649b2c5635449
wilo <sudowilo@gmail.com>
2025-08-08 18:19:36 +0330

    feat(ui/config): more flexible base url for api's from mobile in dev process

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ const __filename = import.meta.filename;
 const __dirname = path.dirname(__filename);
 
 // serving static files (images and ...)
-const clientPath = path.join(__dirname, '../client');
+const clientPath = path.join(__dirname, "../client");
 app.use(express.static(clientPath));
 
 export default app;
