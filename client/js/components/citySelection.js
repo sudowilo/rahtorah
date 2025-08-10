ddedf4b4893076f7371b0f2e115b5b3f562a6ca1
wilo <sudowilo@gmail.com>
2025-08-10 20:37:09 +0330

    feat(ui/select-city): changing bod.innerHTML to insertAdjaccentHTML because it disabled all the events and adding close button to select city panel

--- client/js/components/citySelection.js
+++ client/js/components/citySelection.js
@@ -1,24 +1,35 @@
 export const selectCity = () => {
-  const cityButtons = document.querySelectorAll('.js-city-selector');
-  const body = document.querySelector('body');
-  const innerBody = document.querySelector('.inner-body');
+  const cityButtons = document.querySelectorAll(".js-city-selector");
+  const body = document.querySelector("body");
 
   for (const button of cityButtons) {
-    button.addEventListener('click', (elem)=>{
-      const {cityType} = button.dataset;
-      innerBody.classList.add('blurred');
+    button.addEventListener("click", (elem) => {
+      const { cityType } = button.dataset;
 
-      body.innerHTML = body.innerHTML + `
+      body.insertAdjacentHTML(
+        "beforeend",
+        `
       <div class="city-selection-window prevent-select">
         <div class="top-section">
-          <div class="close-button">✕</div>
+          <div class="close-button js-close-button">✕</div>
           <div class="select-button">استان</div>
           <div class="select-button">شهر</div>
         </div>
         <div class="bottom-section">
           <div class="submit-button">تایید</div>
         </div>
-      </div>`;
-    })
+      </div>`
+      );
+
+      const innerBody = document.querySelector(".inner-body");
+      innerBody.classList.add("blurred");
+
+      const closeButton = document.querySelector(".js-close-button");
+      closeButton.addEventListener("click", () => {
+        innerBody.classList.remove("blurred");
+        const panel = document.querySelector(".city-selection-window");
+        panel.remove();
+      });
+    });
   }
-}
+};
--- client/js/index.js
+++ client/js/index.js
@@ -4,5 +4,9 @@ import { renderUserCard } from "./components/userCard.js";
 document.addEventListener("DOMContentLoaded", async () => {
   const userCardContainer = document.querySelector(".user-card");
   await renderUserCard(userCardContainer);
+  const request = document.querySelector('.requests');
+  request.addEventListener('click', ()=>{
+    console.log('request');
+  })
   selectCity();
 })
