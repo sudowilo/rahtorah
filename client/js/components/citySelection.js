3cc502d98d992c7652eec3c56809816aefc706f3
wilo <sudowilo@gmail.com>
2025-08-10 17:59:14 +0330

    feat(ui/city-select): making button inter active

--- client/js/components/citySelection.js
+++ client/js/components/citySelection.js
@@ -1,8 +1,24 @@
 export const selectCity = () => {
   const cityButtons = document.querySelectorAll('.js-city-selector');
+  const body = document.querySelector('body');
+  const innerBody = document.querySelector('.inner-body');
+
   for (const button of cityButtons) {
     button.addEventListener('click', (elem)=>{
-      console.log('hey you clicked', button.dataset.cityType);
+      const {cityType} = button.dataset;
+      innerBody.classList.add('blurred');
+
+      body.innerHTML = body.innerHTML + `
+      <div class="city-selection-window prevent-select">
+        <div class="top-section">
+          <div class="close-button">✕</div>
+          <div class="select-button">استان</div>
+          <div class="select-button">شهر</div>
+        </div>
+        <div class="bottom-section">
+          <div class="submit-button">تایید</div>
+        </div>
+      </div>`;
     })
   }
 }
