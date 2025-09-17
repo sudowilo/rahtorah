d1be2d50c74e37ad5de1139b7ee9a731ed40c9d4
wilo <sudowilo@gmail.com>
2025-09-17 11:39:26 +0330

    feat(create-trip/adding css and layouts)

--- client/js/components/createTrip.js
+++ client/js/components/createTrip.js
@@ -10,34 +10,55 @@ const tripForm = () => {
     "beforeend",
     `
     <div class="create-trip-panel">
-      <div class="origin-selection">
+      <div class="origin-selection row">
         <div class="origin-label sm-label">مبدا</div>
       </div>
-      <div class="destination-selection">
+      <div class="destination-selection row">
         <div class="destination-label sm-label">مقصد</div>
       </div>
-      <div class="departure-date">
+      <div class="departure-date row">
         <div class="departure-label sm-label">تاریخ حرکت</div>
+        <div class="date-container">
+          <div class="date-year"></div>
+          <div class="date-month"></div>
+          <div class="date-day"></div>
+        </div>
       </div>
-      <div class="departure-time">
+      <div class="departure-time row">
         <div class="right-section">
           <div class="sm-label">زمان حرکت</div>
           <div class="time-specifier">از</div>
+          <div class="time-container">
+            <div class="time-hour"></div>
+            <div class="time-minute"></div>
+          </div>
         </div>
         <div class="left-section">
           <div class="time-specifier">تا</div>
+          <div class="time-container">
+            <div class="time-hour"></div>
+            <div class="time-minute"></div>
+          </div>
         </div>
       </div>
-      <div class="suggested-transport-section">
-        <label for="suggested-transport-service">سرویس حمل و نقل پیشنهادی</label>
-        <input type="text" id="suggested-transport-service" name="suggested-transport-service">
+      <div class="suggested-transport-section row">
+        <label for="suggested-transport-service"
+          >سرویس حمل و نقل پیشنهادی</label
+        >
+        <input
+          type="text"
+          id="suggested-transport-service"
+          name="suggested-transport-service"
+        />
       </div>
-      <div class="passengers-info">
+      <div class="passengers-info row">
         <div class="passengers-number-section">
-          <div class="sm-label">تعداد اشخاص مورد نیاز</div>
+          <div class="sm-label">صندلی خالی</div>
+          <div class="seats-number"></div>
         </div>
         <div class="passengers-gender-section">
           <div class="sm-label">جنسیت</div>
+          <div class="gender-selection"></div>
         </div>
       </div>
       <div class="note-section">
@@ -45,17 +66,19 @@ const tripForm = () => {
           <div class="md-label">یادداشت</div>
           <div class="sm-label">نکاتی که قبل ارسال درخواست بهتر بقیه بدونن</div>
         </div>
-        <textarea name="trip-note-input" id="trip-note-input"></textarea>
+        <div class="note-input-container">
+          <textarea name="trip-note-input" id="trip-note-input"></textarea>
+        </div>
       </div>
       <div class="actions-section">
-        <div class="submit-trip">ثبت درخواست</div>
+        <div class="submit-trip">ثبت</div>
         <div class="cancel">لغو</div>
       </div>
     </div>`
   );
   const originSection = document.querySelector(".origin-selection");
   const destinationSection = document.querySelector(".destination-selection");
-  
+
   citySelectButtons(originSection);
   citySelectButtons(destinationSection);
 };
